d12782d8ee9c157f8ceb950f4d4578c9950eda71
ginanjartg <ginanjar0822@gmail.com>
2023-09-21 00:34:50 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,5 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
-    build: {
-        target: "serverless",
-        outputDir: "build",
-    },
-    plugins: [
-        laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
-            refresh: true,
-        }),
-    ],
+    plugins: [laravel()],
 });
